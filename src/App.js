fc9697a72ac5579315036f512e43090fedf0b881
Celinka Cywińska <celinkacywinska@Celinkas-MacBook-Air.local>
2022-09-21 21:58:45 -0700

    Fix footer

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default ()=> {
   return (
     <div className="App">
       <div className="App-header">
-        <h1>My Chat App</h1>
+        <h1>CS Hero</h1>
       </div>
       <Messages
         messages={messages}
