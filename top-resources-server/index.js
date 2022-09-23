5f64feae2c7a71997f2c6a9aa4dea2a293b17d34
Jorge Sanchez <sandix390@gmail.com>
2022-09-23 15:35:32 -0500

    add youtube enpoint improvements

--- top-resources-server/index.js
+++ top-resources-server/index.js
@@ -5,10 +5,9 @@ const pretty = require("pretty");
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
@@ -19,13 +18,23 @@ app.get("/api/v1", (req, res) => {
 // Youtube link receveing an array of keywords
 app.get("/api/v1/youtube", async (req, res) => {
     const { q } = req.query;
+    if (!q) {
+        res.status(400).json({ message: "Please provide a search query" });
+    }
     const keywords = q.split("+");
     console.log(keywords);
     const results = await youtubesearchapi.GetListByKeyword(keywords);
-    res.json("https://www.youtube.com/watch?v=" + results.items[0].id).status(200);
+    res.json("https://www.youtube.com/watch?v=" + results.items[0].id).status(
+        200
+    );
+});
+
+// wildcard for all other routes
+app.get("*", (req, res) => {
+    res.status(404).json({ message: "Route not found" });
 });
 
 // server listening
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-}); 
+});
