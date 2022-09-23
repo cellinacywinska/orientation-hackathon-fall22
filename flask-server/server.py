15ea3b219e2e5803cb750287d5a37e5d9efe6828
Celinka Cywińska <celinkacywinska@Celinkas-MacBook-Air.local>
2022-09-22 21:14:46 -0700

    implementation of requests

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,19 +1,26 @@
 from urllib import request
 from flask import Flask, request, jsonify, render_template
 from ai import *
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
+
+
 
 # Members API route
 @app.route("/", methods=['POST'])
 def chat():
     if request.method == 'POST':
-        process = cleanComment(request.form['message'])
+        process = cleanComment(request.json["message"])
         if process:
             #Search stackoverflow for related posts. F.ex findPost(process)
-            return jsonify(message=process)
+            response = jsonify(message=process)
+            return response
         else:
-            return jsonify(message="Sorry, we couldn't find anything")
+            response = jsonify(message="Sorry, we couldn't find anything")
+            return response
     else:
         return "error"
 
